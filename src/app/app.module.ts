9d21507938e6eba913388ee5ab940bdc66f19d47
Valter Lobo <valterlobo@gmail.com>
2019-02-26 19:37:36 -0300

    Reports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { CoreModule } from './core/core.module';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 @NgModule({
   declarations: [
     AppComponent
